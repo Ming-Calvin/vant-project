daa2ab7e52d8a6c15eeeeac582f3078e95ea67e8
小明 <2431165604@qq.com>
2024-02-28 01:38:37 +0800

    feat： 新增

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TreeSelect :orgTreeArr="orgTreeArr" :columns="columns"></TreeSelect>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TreeSelect from "@/components/TreeSelect.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {TreeSelect},
+  data() {
+    return {
+      orgTreeArr: [1, 2, 3],
+      columns: [{
+        id: 1,
+        level: 1,
+        name: '1xxx',
+        isShow: false,
+        children: [{
+          id: 3,
+          level: 2,
+          name: '3xxx',
+          isShow: false,
+        }, {
+          id: 4,
+          level: 2,
+          name: '4xxx',
+          isShow: false,
+        }]
+      }, {
+        id: 2,
+        level: 1,
+        name: '2xxx',
+        isShow: false,
+        children: [{
+          id: 5,
+          level: 2,
+          name: '5xxx',
+          isShow: false,
+        }, {
+          id: 6,
+          level: 2,
+          name: '6xxx',
+          isShow: false,
+        }]
+      }]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/TreeSelect.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <van-checkbox-group v-model="orgTreeArr">
+      <div v-for="(item, index) in columns" :key="index">
+        <!--level1-->
+        <div class="level1">
+          <div class="icon" v-if="iconShow(item)" @click="toggleShow(index)">
+            <van-icon name="minus" size="10" v-if="item.isShow"></van-icon>
+            <van-icon name="plus" size="10" v-else></van-icon>
+          </div>
+          <van-checkbox :name="item" icon-size="14px" shape="square"></van-checkbox>
+          <div> {{ item.name }}</div>
+        </div>
+
+        <!--level2-->
+        <div class="level2">
+          <TreeSelect :columns="item.children" :orgTreeArr="orgTreeArr"></TreeSelect>
+          <van-checkbox-group v-model="result">
+            <van-checkbox
+                v-for="(sItem, sIndex) in peopleList[item.id]"
+                :key="sIndex"
+                :name="sItem.value"
+                shape="square"
+            >
+              <div> {{ sItem.name }}</div>
+            </van-checkbox>
+          </van-checkbox-group>
+        </div>
+      </div>
+    </van-checkbox-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TreeSelect',
+  data() {
+    return {
+      orgTreeArr: [1, 2, 3],
+      result: [],
+      peopleList: [[], [], [{
+        name: 'xm',
+        value: 'xm'
+      }, {
+        name: 'xh',
+        value: 'xh'
+      }], [{
+        name: 'xg',
+        value: 'xg'
+      }]]
+    }
+  },
+  props: {
+    // orgTreeArr: { type: Array },
+    columns: { type: Array }
+  },
+  methods: {
+    // 图标展示
+    iconShow(item) {
+      if(item.children && item.children.length > 0) {
+        return true
+      }
+      return false
+    },
+    toggleShow(index) {
+      this.$set(this.columns, index, { ...this.columns[index], isShow: !this.columns[index].isShow })
+    }
+  }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+* {
+  font-size: 20px;
+}
+
+.level1 {
+  width: 100%;
+  height: 20px;
+  margin: 10px 0;
+  background-color: #f2f2f2;
+  display: flex;
+  align-items: center;
+
+  .icon {
+    margin-right: 10px;
+    height: 28px;
+  }
+}
+</style>
